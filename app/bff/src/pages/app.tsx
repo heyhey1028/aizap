fe9638361fb9523f14461f5080441eb33ea490df
tokku5552 <physics.0121@gmail.com>
2026-01-16 09:46:22 +0900

    fix: change moduleresolution from bundler to nodenext

--- app/bff/src/pages/app.tsx
+++ app/bff/src/pages/app.tsx
@@ -1,7 +1,7 @@
 import { Hono } from 'hono';
-import { Top } from './top';
+import { Top } from './top.js';
 
-const web = new Hono();
+const web: Hono = new Hono();
 
 web.get('/top', (c) => {
   return c.html(<Top />);
